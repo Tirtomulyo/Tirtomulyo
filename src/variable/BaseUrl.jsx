b76a8729adfd1ee9417d7e454d7be37a99620de0
Puputwe <werdiningsihpuput@gmail.com>
2022-01-24 12:14:45 +0700

    Updates

--- src/variable/BaseUrl.jsx
+++
@@ -1,9 +0,0 @@
-const image_url = '/tirtomulyo/assets/images/'
-const api_url = 'https://eduwisata.herokuapp.com/api/'
-const contact_url = 'https://wa.me/6282174398682'
-
-export { 
-    image_url,
-    api_url,
-    contact_url
-}
